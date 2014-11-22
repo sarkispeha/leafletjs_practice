1995c2dd1033d0a209aa2a233634a77f6f2236ec
Sarkis Peha <sarkispeha@gmail.com>
2014-11-22 11:53:51 -0700

    going circle crazy

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 $(document).on('ready', function() {
 
-	var map = L.map('map').setView([51.505, -0.09], 13);
+	var map = L.map('map').setView([40.023798, -105.266070], 13);
 
 	L.tileLayer('http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors',
     id: 'examples.map-i875mjb7'
-}).addTo(map);
+	}).addTo(map);
+
+	var marker = L.marker([40.023798, -105.266070]).addTo(map);
+
+	var circle = L.circle([40.023798, -105.266070], 500, {
+		color: 'blue',
+		fillColor: 'lightblue',
+		fillOpacity: 0.5
+	}).addTo(map);
   
-});
+});//end of jQuery
