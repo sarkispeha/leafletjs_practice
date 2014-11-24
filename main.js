a17aaa8bdf55ad84aa90397a68640dfe20ae002f
Sarkis Peha <sarkispeha@gmail.com>
2014-11-24 09:07:50 -0700

    event handlers and such added to markers

--- main.js
+++ main.js
@@ -7,12 +7,28 @@ $(document).on('ready', function() {
     id: 'examples.map-i875mjb7'
 	}).addTo(map);
 
+//set marker
 	var marker = L.marker([40.023798, -105.266070]).addTo(map);
 
+//create a circle
 	var circle = L.circle([40.023798, -105.266070], 500, {
 		color: 'blue',
 		fillColor: 'lightblue',
 		fillOpacity: 0.5
 	}).addTo(map);
+
+//event for circle click
+	circle.bindPopup("I am a circle");
+
+	var popup = L.popup();
+
+	function onMapClick(e) {
+	    popup
+	        .setLatLng(e.latlng)
+	        .setContent("You clicked the map at " + e.latlng.toString())
+	        .openOn(map);
+	}
+
+	map.on('click', onMapClick);
   
 });//end of jQuery
